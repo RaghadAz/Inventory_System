4ce159cdd3dce5c75183f1f8ab8803a79dfd1365
Raghad Al Azameh <raghads200@gmail.com>
2026-08-10 18:44:06 +0300

    .

--- storage/framework/views/e939fa38d7ea23cdb8d61d971905cb39.php
+++
@@ -1,5 +0,0 @@
-<div <?php echo e($attributes->class(['fi-dropdown-list'])); ?>>
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH D:\laragon\www\ipi405\vendor\filament\support\resources\views/components/dropdown/list/index.blade.php ENDPATH**/ ?>
