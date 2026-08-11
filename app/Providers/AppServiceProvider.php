1cdc930e934c7129d8f22ad4527967a9cdbe1e70
Raghad Al Azameh <raghads200@gmail.com>
2026-08-11 16:36:14 +0300

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,12 +4,16 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Models\Sale;
-use App\Observers\SaleObserver; 
+use App\Observers\SaleObserver;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
         Sale::observe(SaleObserver::class);
     }
 }
