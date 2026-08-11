dad4280e82cb4b13b02f6c5a6c26587b0f825dbb
Raghad Al Azameh <raghads200@gmail.com>
2026-08-11 19:14:58 +0300

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,8 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        URL::forceScheme('https');
+        if (env('APP_ENV') === 'production')
+            URL::forceScheme('https');
 
         Sale::observe(SaleObserver::class);
     }
