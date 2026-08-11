ab29ba7d6cc20f4eb21878c8a31457f355e3a849
Raghad Al Azameh <raghads200@gmail.com>
2026-08-11 19:34:34 +0300

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\URL;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        URL::forceScheme('https');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
